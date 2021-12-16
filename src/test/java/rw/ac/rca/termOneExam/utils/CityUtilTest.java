968049f57d24d4115163033dcd7c817e96df1163
patrickniyogitare28 <patrickniyogitare28@gmail.com>
2021-12-16 17:13:19 +0200

    chore: polishing feature

--- src/test/java/rw/ac/rca/termOneExam/utils/CityUtilTest.java
+++ src/test/java/rw/ac/rca/termOneExam/utils/CityUtilTest.java
@@ -1,12 +1,7 @@
 package rw.ac.rca.termOneExam.utils;
 
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,19 +9,9 @@ import rw.ac.rca.termOneExam.domain.City;
 import rw.ac.rca.termOneExam.repository.ICityRepository;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.anyInt;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.atMost;
-
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 @DataJpaTest
